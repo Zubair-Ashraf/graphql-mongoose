7b22b0796d4595401b460c48c052a5bde139490e
zubair-ashraf <zubair8767844@gmail.com>
2020-10-04 17:00:13 +0500

    grapqhl setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { connectDB } = require("./db");
+const schema = require("./graphql/schema");
+const { graphqlHTTP } = require("express-graphql");
 const app = express();
 dotenv.config();
 connectDB();
@@ -8,6 +10,14 @@ connectDB();
 app.get("", (req, res) => {
   res.json({ ms: "hello" });
 });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App is running on port: ${process.env.PORT}`);
